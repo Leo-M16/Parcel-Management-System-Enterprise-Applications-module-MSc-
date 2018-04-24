ec6c8fd16a6e1c191f310914db70a81df3db1d74
leo-m16 <leomatousian@gmail.com>
2018-04-24 01:50:11 +0100

    fixing a problem

--- JEE7 Source Code/ParcelMSJ-ejb/build/generated-sources/ap-source-output/entity/Vans_.java
+++ JEE7 Source Code/ParcelMSJ-ejb/build/generated-sources/ap-source-output/entity/Vans_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-23T16:41:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-24T01:44:46")
 @StaticMetamodel(Vans.class)
 public class Vans_ { 
 
